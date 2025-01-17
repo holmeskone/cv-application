d623c3aea9207e9a076f8ba37caf48c7ec214471
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-17 11:54:36 +0100

    Added basic inputs for CV generator

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,34 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Accordion
       title={'Personal Information'}
-      inputCount={2}
-      inputText ={['First Name', 'Last Name', ]}
+      inputCount={6}
+      inputText ={['First Name', 'Last Name', 'Telephone', 'Email', 'Nationality', 'Socials']}
     /> 
+    <Accordion
+      title={'Languages'}
+      inputCount={2}
+      inputText ={['Languages', 'Level']}
+    /> 
+    <Accordion
+      title={'About you'}
+      inputCount={2}
+      inputText ={['About you', 'How can you help the team?']}
+    /> 
+    <Accordion
+      title={'Work Experience'}
+      inputCount={6}
+      inputText ={['Company Name', 'Position', 'Location', 'Start Month', 'End Month', 'Achievements']}
+    /> 
+    <Accordion
+      title={'Education'}
+      inputCount={3}
+      inputText ={['University Name', 'Degree', 'Achievements']}
+    /> 
+    <Accordion
+      title={'Skills, Tools and/or Interests'}
+      inputCount={3}
+      inputText ={['Skills', 'Tools', 'Interests']}
+    /> 
+    
   </StrictMode>,
 )

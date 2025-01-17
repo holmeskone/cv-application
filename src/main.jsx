d65d2d19aa23c0b22bdf45b84f9f400c1e09b2a6
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-17 15:20:12 +0100

    Add divs for easier styling of input section and display section

--- src/main.jsx
+++ src/main.jsx
@@ -6,36 +6,42 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Accordion
-      title={'Personal Information'}
-      inputCount={6}
-      inputText ={['First Name', 'Last Name', 'Telephone', 'Email', 'Nationality', 'Socials']}
-    /> 
-    <Accordion
-      title={'Languages'}
-      inputCount={2}
-      inputText ={['Languages', 'Level']}
-    /> 
-    <Accordion
-      title={'About you'}
-      inputCount={2}
-      inputText ={['About you', 'How can you help the team?']}
-    /> 
-    <Accordion
-      title={'Work Experience'}
-      inputCount={6}
-      inputText ={['Company Name', 'Position', 'Location', 'Start Month', 'End Month', 'Achievements']}
-    /> 
-    <Accordion
-      title={'Education'}
-      inputCount={3}
-      inputText ={['University Name', 'Degree', 'Achievements']}
-    /> 
-    <Accordion
-      title={'Skills, Tools and/or Interests'}
-      inputCount={3}
-      inputText ={['Skills', 'Tools', 'Interests']}
-    /> 
-    
+    <div id='container'>
+      <div id='input-section'>
+      <Accordion
+        title={'Personal Information'}
+        inputCount={6}
+        inputText ={['First Name', 'Last Name', 'Telephone', 'Email', 'Nationality', 'Socials']}
+      /> 
+      <Accordion
+        title={'Languages'}
+        inputCount={2}
+        inputText ={['Languages', 'Level']}
+      /> 
+      <Accordion
+        title={'About you'}
+        inputCount={2}
+        inputText ={['About you', 'How can you help the team?']}
+      /> 
+      <Accordion
+        title={'Work Experience'}
+        inputCount={6}
+        inputText ={['Company Name', 'Position', 'Location', 'Start Month', 'End Month', 'Achievements']}
+      /> 
+      <Accordion
+        title={'Education'}
+        inputCount={3}
+        inputText ={['University Name', 'Degree', 'Achievements']}
+      /> 
+      <Accordion
+        title={'Skills, Tools and/or Interests'}
+        inputCount={3}
+        inputText ={['Skills', 'Tools', 'Interests']}
+      /> 
+      </div>
+      <div id='display-section'>
+        <h2>Here is the cV</h2>
+      </div>
+    </div>
   </StrictMode>,
 )
